5c9fa73c73e8cf72c0e0ff6cb538d1205d0609a3
AtulxD <110067334+AtulxD@users.noreply.github.com>
2023-04-22 12:27:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ load_dotenv("config.env")
 API_ID = int(getenv("API_ID", 7038415))
 API_HASH = getenv("API_HASH", "1f914aa1da15ca6153bbcf09ec1414dc")
 BOT_TOKEN = getenv("BOT_TOKEN", "6089831984:AAEAMXZaaRzd0NEb15gHtCudayZllAUxbwY")
-OPENAI_API = getenv("OPENAI_API", "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") # get api key : https://platform.openai.com/account/api-keys
+OPENAI_API = getenv("OPENAI_API", "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") # get api key : https://platform.openai.com/account/api-keys
